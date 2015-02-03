896335845185bda7ffceef07722a6e41720693b3
Ryan Pope <ryan@ryanpope.com>
2015-02-03 12:30:34 -0600

    Added example config

---
+++ util/BaseSettings.example.py
@@ -0,0 +1,9 @@
+__author__ = 'Ryan'
+
+class Settings:
+    def __init__(self):
+        self.Twitch_Username = 'username'
+        self.Twitch_Password = 'oauth:xxx'
+        self.Twitch_Channel = 'TheChannel'
+        self.IRC_Server = 'irc.twitch.tv'
+        self.Trigger = '!'
