a74f2c8ef7d14c62ad8a13bdce388238e6a851c0
Ryan Pope <ryan@ryanpope.com>
2015-02-03 12:13:35 -0600

    Remove ignored files

--- util/BaseSettings.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Ryan'
-
-class Settings:
-    def __init__(self):
-        self.Twitch_Username = 'minipope'
-        self.Twitch_Password = 'oauth:sj0h3v77ps4n3bo4pmbj31um90tm81'
-        self.Twitch_Channel = 'PopeTheThird'
-        self.IRC_Server = 'irc.twitch.tv'
-        self.Trigger = '!'
