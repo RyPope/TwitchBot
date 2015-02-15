d8bb4393b39f80d20e2f501cbe654ae74243313f
Ryan <ryan@ryanpope.com>
2015-02-15 14:27:45 -0600

    Added join/part handlers to logging plugin.

--- plugins/LoggingPlugin/LoggingQueryHelper.py
+++ plugins/LoggingPlugin/LoggingQueryHelper.py
@@ -3,6 +3,8 @@ from database.SQLHelper import SQLHelper
 from database.BaseQueryHelper import BaseQueryHelper
 from contextlib import closing
 import traceback
+from objects.message import Message
+import datetime
 
 class LoggingQueryHelper():
     def __init__(self):
@@ -21,6 +23,18 @@ class LoggingQueryHelper():
                     "`msg` text,"
                     "`timestamp` datetime DEFAULT CURRENT_TIMESTAMP,"
                     "PRIMARY KEY (`message_id`))")
+
+                cur.execute("CREATE TABLE IF NOT EXISTS `joins` "
+                            "(`user_id` int NOT NULL,"
+                            "`channel_id` int NOT NULL,"
+                            "`joined` datetime NOT NULL,"
+                            "PRIMARY KEY (`user_id`, `channel_id`))")
+
+                cur.execute("CREATE TABLE IF NOT EXISTS `time_spent`"
+                            "(`user_id` int NOT NULL,"
+                            "`channel_id` int NOT NULL,"
+                            "`time_spent` int NOT NULL DEFAULT 0,"
+                            "PRIMARY KEY (`user_id`, `channel_id`))")
                 db.commit()
 
         except Exception as e:
@@ -68,3 +82,59 @@ class LoggingQueryHelper():
         finally:
             db.close()
             return messages
+
+    def insertJoined(self, username, channel, time):
+        try:
+            db = self.sqlHelper.getConnection()
+            with closing(db.cursor()) as cur:
+                channel_id = self.queryHelper.getChannelID(channel)
+                user_id = self.queryHelper.getUserID(username)
+
+                cur.execute("""INSERT IGNORE INTO `joins` (`channel_id`, `user_id`, `joined`) VALUES(%s, %s, %s)""", (channel_id, user_id, time))
+                db.commit()
+
+        except Exception as e:
+            print("Error inserting new join")
+            print(traceback.format_exc())
+        finally:
+            db.close()
+
+    def popJoined(self, username, channel):
+        joinTime = datetime.datetime.now()
+        try:
+            db = self.sqlHelper.getConnection()
+
+            with closing(db.cursor()) as cur:
+                channel_id = self.queryHelper.getChannelID(channel)
+                user_id = self.queryHelper.getUserID(username)
+
+                cur.execute("""SELECT joined FROM `joins` WHERE `channel_id` = %s AND `user_id` = %s""", (channel_id, user_id))
+                joined = cur.fetchone()
+
+                if not joined is None:
+                    joinTime = joined[0]
+
+                cur.execute("""DELETE FROM joins WHERE user_id = %s AND channel_id = %s""", (user_id, channel_id))
+                db.commit()
+
+        except Exception as e:
+            print(traceback.format_exc())
+        finally:
+            db.close()
+            return joinTime
+
+    def updateTimeSpent(self, username, channel, time):
+        try:
+            db = self.sqlHelper.getConnection()
+            with closing(db.cursor()) as cur:
+                channel_id = self.queryHelper.getChannelID(channel)
+                user_id = self.queryHelper.getUserID(username)
+
+                cur.execute("""UPDATE `time_spent` SET `time_spent` = `time_spent` + %s WHERE `user_id` = %s AND `channel_id` = %s""", (time, user_id, channel_id))
+                db.commit()
+
+        except Exception as e:
+            print("Error inserting new time spent")
+            print(traceback.format_exc())
+        finally:
+            db.close()
--- plugins/LoggingPlugin/plugin.py
+++ plugins/LoggingPlugin/plugin.py
@@ -1,5 +1,6 @@
 from plugins.BasePlugin import BasePlugin
 from plugins.LoggingPlugin.LoggingQueryHelper import LoggingQueryHelper
+import datetime
 
 class LoggingPlugin(BasePlugin):
     def __init__(self, twitchBot):
@@ -9,13 +10,32 @@ class LoggingPlugin(BasePlugin):
 
         self.registerAll(self.className, self.messageHandler)
         self.registerJoinPartNotifications(self.className, self.joinPartHandler)
-        self.registerCommand(self.className, "mystats", self.myStatsHandler)
+        self.registerCommand(self.className, "stats", self.myStatsHandler)
+        self.registerCommand(self.className, "testpart", self.testPartHandler)
+
+        self.joinList = []
+
+    def testPartHandler(self, username, channel, args):
+        joinTime = self.loggingQueryHelper.popJoined(username, channel)
+        diff = datetime.datetime.now() - joinTime
+        timeSpent = divmod(diff.days * 86400 + diff.seconds, 60)
+        self.loggingQueryHelper.updateTimeSpent(username, channel, timeSpent[0])
+        print(self.className, channel, "%s spent %d minutes, %d seconds in %s" % (username, timeSpent[0], timeSpent[1], channel))
 
     def messageHandler(self, username, channel, args):
         self.loggingQueryHelper.insertMsg(username, channel, " ".join(args))
 
     def joinPartHandler(self, username, channel, isJoin):
         self.loggingQueryHelper.insertUser(username)
+
+        if isJoin:
+            self.loggingQueryHelper.insertJoined(username, channel, datetime.datetime.now())
+        else:
+            joinTime = self.loggingQueryHelper.popJoined(username, channel)
+            diff = datetime.datetime.now() - joinTime
+            timeSpent = divmod(diff.days * 86400 + diff.seconds, 60)
+            self.loggingQueryHelper.updateTimeSpent(username, channel, timeSpent[0])
+            print(self.className, channel, "%s spent %d minutes, %d seconds in %s" % (username, timeSpent[0], timeSpent[1], channel))
 
     def myStatsHandler(self, username, channel, args):
         chatMessages = self.loggingQueryHelper.getMessages(username, channel)
